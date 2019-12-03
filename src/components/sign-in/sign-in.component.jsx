fa61f67c9e284415b83eaa96a124db11abf12ddc
Nidhal BEN TAHAR <nidhal.ben.tahar@gmail.com>
2019-12-03 22:35:59 +0100

    feat: implement google sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -51,8 +51,12 @@ class SignIn extends React.Component {
             label='password'
             required
           />
-          <CustomButton type='submit'> Sign in </CustomButton>
-          <CustomButton onClick={ signInWithGoogle }> Sign in With Google </CustomButton>
+          <div className="buttons">
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in With Google
+              </CustomButton>
+          </div>
         </form>
       </div>
     );
