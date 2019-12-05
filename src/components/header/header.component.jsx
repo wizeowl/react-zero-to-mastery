43ad07f2aceb62a59c058039f550daad260cca02
Nidhal BEN TAHAR <nidhal.ben.tahar@gmail.com>
2019-12-05 21:08:20 +0100

    feat: implement cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,7 @@ import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
+import Cart from '../cart-dropdown/cart-dropdown.component';
 import CartIcon from '../cart-icon/cart-icon.component';
 
 const Header = ({ currentUser }) => (
@@ -28,6 +29,7 @@ const Header = ({ currentUser }) => (
 
       <CartIcon/>
     </div>
+    <Cart/>
   </div>
 );
 
