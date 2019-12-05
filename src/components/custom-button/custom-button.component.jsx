7e8dc7c58423636bc1f1a86add872ecfd2b2e1be
Nidhal BEN TAHAR <nidhal.ben.tahar@gmail.com>
2019-12-05 22:14:38 +0100

    feat: implement add to cart button

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import './custom-buttom.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-  <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, isInverted, ...otherProps }) => (
+  <button className={`
+    ${isGoogleSignIn ? 'google-sign-in' : ''}
+    ${isInverted ? 'inverted' : ''}
+    custom-button`} {...otherProps}>
     {children}
   </button>
 );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -33,7 +33,7 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+const mapStateToProps = ({  user: { currentUser }, cart: { hidden } }) => ({
   currentUser, hidden
 });
 
